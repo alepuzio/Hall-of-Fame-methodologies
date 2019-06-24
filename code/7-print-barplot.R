a37b71bdd56e0a949bb6f61ef58ecd13895ef3ed
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2019-06-25 01:01:53 +0200

    allignment of assignment

--- code/7-print-barplot.R
+++ code/7-print-barplot.R
@@ -2,13 +2,13 @@
 #print barplot
 xx <- barplot(raw.mean.to.print$TRAFFIC,
              args.legend = list(x = "topleft"),
-             col = rainbow(3),
+             col         = rainbow(3),
              legend.text = raw.mean.to.print$TRAFFIC,
-             main = "ANNUAL MEANS BETWEEN 2018 FEB AND 2019 JUN",
-             names.arg = raw.mean.to.print$METHODOLOGY,
-             xlab = "Methodology",
-             ylab = "Volume",
-             ylim = c(0,100),
-             width = 1
+             main        = "ANNUAL MEANS BETWEEN 2018 FEB AND 2019 JUN",
+             names.arg   = raw.mean.to.print$METHODOLOGY,
+             xlab        = "Methodology",
+             ylab        = "Volume",
+             ylim        = c(0,100),
+             width       = 1
 )
 
