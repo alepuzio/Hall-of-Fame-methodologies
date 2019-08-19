cf4d8c61ff96fe732004a14697d4ad2c9b361ab8
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2019-08-20 00:52:26 +0200

    minor change

--- R/8-2-par-hist.R
+++ R/8-2-par-hist.R
@@ -1,8 +1,8 @@
 #8-2-par-hist
 par(mfrow=c(1,3))
-hist(raw.data.interesting$KANBAN, col = "red", main = "KANBAN", xlab = "VOLUME", ylim = c(0, 40))
-hist(raw.data.interesting$SCRUM, col = "green", main = "SCRUM", xlab = "VOLUME", ylim = c(0, 40))
-hist(raw.data.interesting$NOESTIMATES, col = "blue", main = "NOESTIMATES", xlab = "VOLUME", ylim = c(0, 40))
+hist(raw.data.interesting$KANBAN, col = "red", main = "KANBAN", xlab = "VOLUME VALUES", ylim = c(0, 40))
+hist(raw.data.interesting$SCRUM, col = "green", main = "SCRUM", xlab = "VOLUME VALUES", ylim = c(0, 40))
+hist(raw.data.interesting$NOESTIMATES, col = "blue", main = "NOESTIMATES", xlab = "VOLUME VALUES", ylim = c(0, 40))
 
 
 #barplot(prop.table(table(raw.data.interesting )))                              
