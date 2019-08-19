ac9515a53d23196b70c3a1cff9e79896f3da5463
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2019-08-20 00:49:08 +0200

    updated path: from relative to absolute

--- R/4-save-data.R
+++ R/4-save-data.R
@@ -1,5 +1,7 @@
 #save data in hard disk
-file.to.write <- ".\\output\\2018-19-means-of-diffusion-of-software-development-methodologies.csv"
+#C:/repo-progetti-R/Hall-of-Fame-methodologies/
+#..\\output\\2018-19-means-of-diffusion-of-software-development-methodologies.csv
+file.to.write <- "C:\\repo-progetti-R\\Hall-of-Fame-methodologies\\output\\2018-19-means-of-diffusion-of-software-development-methodologies.csv"
 
 write.csv(
   mean.values.ordered,
